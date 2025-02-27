0bb38a341328a23d1e368cdb7f2c85d538aa168e
SarikViper <dzamolovsardor5@gmail.com>
2025-02-27 18:54:59 +0500

    tsconfigs configurated

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import "./App.css"
+import './App.css';
 import { useEffect } from "react";
 import { useState } from "react";
 
+interface user {
+  id: number;
+  name: string;
+  username: string;
+  phone: string;
+  email: string;
+  website: string;
+}
+
+
 function App() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<user[]>([]);
   const [modal, setModal] = useState(false);
-  const [selectedUser, setUser] = useState(null);
+  const [selectedUser, setUser] = useState<user | null>(null);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((res) => setUsers(res)); // promise x3
+      .then((res: Response) => res.json())
+      .then((res: user[]) => setUsers(res)); // promise x3
   }, []);
 
   return (
@@ -18,11 +28,11 @@ function App() {
 
       {modal && <div className="modal">
         <div className="selected_user">
-          <h1>Name: {selectedUser.name}</h1>
-          <p><b>Username:</b> {selectedUser.username}</p>
-          <p><b>Phone number:</b> {selectedUser.phone}</p>
-          <p><b>Gmail adress:</b> {selectedUser.email}</p>
-          <p><b>Website:</b> {selectedUser.website}</p>
+          <h1>Name: {selectedUser?.name}</h1>
+          <p><b>Username:</b> {selectedUser?.username}</p>
+          <p><b>Phone number:</b> {selectedUser?.phone}</p>
+          <p><b>Gmail adress:</b> {selectedUser?.email}</p>
+          <p><b>Website:</b> {selectedUser?.website}</p>
         </div>
         <button onClick={() => { setModal(false); setUser(null) }}>Back</button>
       </div>}
